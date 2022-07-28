18b6025f3c583365f0c14afa77350c06f6c3aa51
Leopold Johannes Meinel <leo@meinel.dev>
2022-07-28 23:38:34 +0200

    Fetch Upstream

---
+++ commit.sh
@@ -0,0 +1,122 @@
+#!/bin/sh
+
+cd /home/user/src/Java/papermc_plugins/DropConfirm
+git add .
+git commit -m "Update copyright headers"
+git push
+cd /home/user/src/Java/papermc_plugins/GBooster
+git add .
+git commit -m "Update copyright headers"
+git push
+cd /home/user/src/Java/papermc_plugins/RandomSpawnTp
+git add .
+git commit -m "Update copyright headers"
+git push
+cd /home/user/src/Java/papermc_plugins/SpecPlayer
+git add .
+git commit -m "Update copyright headers"
+git push
+cd /home/user/src/Java/papermc_plugins/vital/VitalBack
+git add .
+git commit -m "Update copyright headers"
+git push
+cd /home/user/src/Java/papermc_plugins/vital/VitalBroadcast
+git add .
+git commit -m "Update copyright headers"
+git push
+cd /home/user/src/Java/papermc_plugins/vital/VitalCondense
+git add .
+git commit -m "Update copyright headers"
+git push
+cd /home/user/src/Java/papermc_plugins/vital/VitalCraft
+git add .
+git commit -m "Update copyright headers"
+git push
+cd /home/user/src/Java/papermc_plugins/vital/VitalEnchant
+git add .
+git commit -m "Update copyright headers"
+git push
+cd /home/user/src/Java/papermc_plugins/vital/VitalFeed
+git add .
+git commit -m "Update copyright headers"
+git push
+cd /home/user/src/Java/papermc_plugins/vital/VitalFix
+git add .
+git commit -m "Update copyright headers"
+git push
+cd /home/user/src/Java/papermc_plugins/vital/VitalFly
+git add .
+git commit -m "Update copyright headers"
+git push
+cd /home/user/src/Java/papermc_plugins/vital/VitalHeal
+git add .
+git commit -m "Update copyright headers"
+git push
+cd /home/user/src/Java/papermc_plugins/vital/VitalHome
+git add .
+git commit -m "Update copyright headers"
+git push
+cd /home/user/src/Java/papermc_plugins/vital/VitalKeepExp
+git add .
+git commit -m "Update copyright headers"
+git push
+cd /home/user/src/Java/papermc_plugins/vital/VitalKeepInv
+git add .
+git commit -m "Update copyright headers"
+git push
+cd /home/user/src/Java/papermc_plugins/vital/VitalMail
+git add .
+git commit -m "Update copyright headers"
+git push
+cd /home/user/src/Java/papermc_plugins/vital/VitalMotd
+git add .
+git commit -m "Update copyright headers"
+git push
+cd /home/user/src/Java/papermc_plugins/vital/VitalPlayerTime
+git add .
+git commit -m "Update copyright headers"
+git push
+cd /home/user/src/Java/papermc_plugins/vital/VitalPlayerWeather
+git add .
+git commit -m "Update copyright headers"
+git push
+cd /home/user/src/Java/papermc_plugins/vital/VitalRules
+git add .
+git commit -m "Update copyright headers"
+git push
+cd /home/user/src/Java/papermc_plugins/vital/VitalSeen
+git add .
+git commit -m "Update copyright headers"
+git push
+cd /home/user/src/Java/papermc_plugins/vital/VitalSkull
+git add .
+git commit -m "Update copyright headers"
+git push
+cd /home/user/src/Java/papermc_plugins/vital/VitalSpawn
+git add .
+git commit -m "Update copyright headers"
+git push
+cd /home/user/src/Java/papermc_plugins/vital/VitalSuicide
+git add .
+git commit -m "Update copyright headers"
+git push
+cd /home/user/src/Java/papermc_plugins/vital/VitalTp
+git add .
+git commit -m "Update copyright headers"
+git push
+cd /home/user/src/Java/papermc_plugins/vital/VitalTpa
+git add .
+git commit -m "Update copyright headers"
+git push
+cd /home/user/src/Java/papermc_plugins/vital/VitalTrash
+git add .
+git commit -m "Update copyright headers"
+git push
+cd /home/user/src/Java/papermc_plugins/vital/VitalWarp
+git add .
+git commit -m "Update copyright headers"
+git push
+cd /home/user/src/Java/papermc_plugins/vital/VitalWelcome
+git add .
+git commit -m "Update copyright headers"
+git push
